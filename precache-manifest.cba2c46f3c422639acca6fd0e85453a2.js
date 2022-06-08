80de85f58bcaaa381220b63df069f785a77172df
AdiPlay <kokoszka.adrian2020@gmail.com>
2022-06-08 16:26:55 +0200

    todo

---
+++ precache-manifest.cba2c46f3c422639acca6fd0e85453a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8f2faef052bedb7d20f",
+    "url": "/todolist/static/css/main.55f599fd.chunk.css"
+  },
+  {
+    "revision": "a8f2faef052bedb7d20f",
+    "url": "/todolist/static/js/main.a8f2faef.chunk.js"
+  },
+  {
+    "revision": "9af0be3872da71a5982a",
+    "url": "/todolist/static/js/1.9af0be38.chunk.js"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "f833039371bafc195e0a51ca371c48df",
+    "url": "/todolist/index.html"
+  }
+];
